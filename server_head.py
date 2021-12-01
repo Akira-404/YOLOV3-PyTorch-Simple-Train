1da90ae277e7c143117bd327cdb2778bd6d95ed9
lee linfeng <2033211717@qq.com>
2021-12-01 17:22:48 +0800

    fixbug

--- server_head.py
+++ server_head.py
@@ -46,17 +46,11 @@ def _get_result(code: int, message: str, data):
 
 
 @app.route('/yolov3_get_head', methods=['POST'])
-def get_person():
-    # img = Image.open('img.jpg')
+def get_head():
     params = request.json if request.method == "POST" else request.args
     img = _base64_to_pil(params['img'])
-    # img.show()
     data = predict.tiny_detect_image(img)
-    for i, item in enumerate(data):
-        print(i, item)
-    return _get_result(200,
-                       'success',
-                       data)
+    return _get_result(200, 'success', data)
 
 
 def run():
